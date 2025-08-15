4fa669ca6cbc2f5c50269a388c2327d42b851fd2
vmelikyan <vahan.melikyan@gmail.com>
2025-08-14 19:36:31 -0700

    try 8

--- src/server/lib/nativeBuild/engines.ts
+++ src/server/lib/nativeBuild/engines.ts
@@ -89,9 +89,9 @@ const ENGINES: Record<string, BuildEngine> = {
         '--output',
         `type=image,name=${destination},push=true,registry.insecure=true,oci-mediatypes=false`,
         '--export-cache',
-        `type=registry,ref=${cacheRef},mode=max,registry.insecure=true`,
+        `type=registry,ref=${cacheRef},mode=max,registry.insecure=false`,
         '--import-cache',
-        `type=registry,ref=${cacheRef},registry.insecure=true`,
+        `type=registry,ref=${cacheRef},registry.insecure=false`,
       ];
 
       Object.entries(buildArgs).forEach(([key, value]) => {
